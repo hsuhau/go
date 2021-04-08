4cacb2021455b4c9690b7b8ca9edf662823102a6
hsuhau <hsuhau@foxmail.com>
2021-04-08 15:27:42 +0800

    go basics

--- src/go_basics.go
+++ src/go_basics.go
@@ -56,6 +56,20 @@ var (
 
 const Pi = 3.14
 
+const (
+	// 将 1 左移 100 位来创建一个非常大的数字
+	// 即这个数的二进制是 1 后面跟着 100 个 0
+	Big = 1 << 100
+	// 再往右移 99 位，即 Small = 1 << 1，或者说 Small = 2
+	Small = Big >> 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
 func main() {
 	fmt.Println("that's12 not cool!")
 
@@ -231,5 +245,7 @@ func main() {
 	（int 类型最大可以存储一个 64 位的整数，有时会更小。）
 
 	（int 可以存放最大64位的整数，根据平台不同有时会更少。）*/
-
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
